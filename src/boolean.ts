cf3d81cb315e6e3045d9fe15665c298fdcb248df
soregashi-27 <tomo.soul.15@gmail.com>
2022-01-17 21:30:43 +0900

    コメントアウト

--- src/boolean.ts
+++ src/boolean.ts
@@ -1,8 +1,11 @@
 export {};
 
-let name = 'TypeScript';
+// let name = 'TypeScript';
+
+// let isFinished: boolean = true;
+// isFinished = false;
+// // isFinished = 1;
+// console.log({ isFinished });
 
 let isFinished: boolean = true;
-isFinished = false;
-// isFinished = 1;
 console.log({ isFinished });
