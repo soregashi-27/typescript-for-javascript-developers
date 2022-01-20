5fd9ce417f5cd124f18886269c66aefc7c541c30
soregashi-27 <tomo.soul.15@gmail.com>
2022-01-20 22:02:22 +0900

    arrayのoutput確認

--- src/array.ts
+++ src/array.ts
@@ -1,7 +1,20 @@
 export {};
 
-let numbers: number[] = [1, 2, 3];
+// let numbers: number[] = [1, 2, 3];
 
+// let numbers2: Array<number> = [1, 2, 3];
+// let strings2: Array<string> = ['Tokyo', 'Osaka', 'Kyoto'];
+
+// let strings: string[] = ['TypeScript', 'JavaScript', 'CoffeeScript'];
+
+// let nijigenHairetsu: number[][] = [
+//   [50, 100],
+//   [150, 300]
+// ];
+
+// let hairetsu: (string | number | boolean)[] = [1, false, 'Japan'];
+
+let numbers: number[] = [1, 2, 3];
 let numbers2: Array<number> = [1, 2, 3];
 let strings2: Array<string> = ['Tokyo', 'Osaka', 'Kyoto'];
 
@@ -9,7 +22,7 @@ let strings: string[] = ['TypeScript', 'JavaScript', 'CoffeeScript'];
 
 let nijigenHairetsu: number[][] = [
   [50, 100],
-  [150, 300]
+  [150, 300],
 ];
 
 let hairetsu: (string | number | boolean)[] = [1, false, 'Japan'];
