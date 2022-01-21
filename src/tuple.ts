6fc615e1f81fccd2800d8d18f358ce55e9c1b43a
soregashi-27 <tomo.soul.15@gmail.com>
2022-01-22 00:45:09 +0900

    tuple型の挙動を確認

--- src/tuple.ts
+++ src/tuple.ts
@@ -1,4 +1,9 @@
 export {};
 
+//let profile: [string, number] = ['Ham', 43];
+// profile = [43, 'Ham'];
+
+//tuple型は制約のレベルを高く出来る
 let profile: [string, number] = ['Ham', 43];
 // profile = [43, 'Ham'];
+// ↑このケースにするとエラーが出る（tuple型でannotationを定義しているため）
