6894f34348de6288f402fcef55f251d2f6c3a14f
soregashi-27 <tomo.soul.15@gmail.com>
2022-01-21 21:13:09 +0900

    コメントの追加

--- src/array.ts
+++ src/array.ts
@@ -15,9 +15,13 @@ export {};
 // let hairetsu: (string | number | boolean)[] = [1, false, 'Japan'];
 
 let numbers: number[] = [1, 2, 3];
+
+// 非推奨の書き方
 let numbers2: Array<number> = [1, 2, 3];
 let strings2: Array<string> = ['Tokyo', 'Osaka', 'Kyoto'];
+// 非推奨の書き方 fin
 
+// アノテーション
 let strings: string[] = ['TypeScript', 'JavaScript', 'CoffeeScript'];
 
 let nijigenHairetsu: number[][] = [
