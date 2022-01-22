d38c9d2c9ab55cd7d7061db87bd4622b5c5039c7
soregashi-27 <tomo.soul.15@gmail.com>
2022-01-22 22:30:39 +0900

    axiosの使用準備

--- src/any.ts
+++ src/any.ts
@@ -15,3 +15,9 @@
 //   data = response.data;
 //   console.log(data);
 // });
+
+import axios from 'axios';
+export {};
+
+let url: string =
+  'https://udemy-utils.herokuapp.com/api/v1/articles?token=token123';
