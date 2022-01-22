b826c6007b570d64b28a03dd56c8e67ce6d40ac1
soregashi-27 <tomo.soul.15@gmail.com>
2022-01-22 22:24:51 +0900

    コメントアウトとaxiosのインストール、最新verを確認

--- src/any.ts
+++ src/any.ts
@@ -1,17 +1,17 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export {};
+// export {};
 
-let url: string =
-  'https://udemy-utils.herokuapp.com/api/v1/articles?token=token123';
+// let url: string =
+//   'https://udemy-utils.herokuapp.com/api/v1/articles?token=token123';
 
-axios.get(url).then(function(response) {
-  interface Article {
-    id: number;
-    title: string;
-    description: string;
-  }
-  let data: Article[];
-  data = response.data;
-  console.log(data);
-});
+// axios.get(url).then(function(response) {
+//   interface Article {
+//     id: number;
+//     title: string;
+//     description: string;
+//   }
+//   let data: Article[];
+//   data = response.data;
+//   console.log(data);
+// });
